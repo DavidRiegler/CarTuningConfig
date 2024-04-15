71300361c0ac0481cc522e72e9f19a9eb5640ef5
DavidRiegler <111269612+DavidRiegler@users.noreply.github.com>
2024-04-15 16:01:43 +0200

    CarOverview adjustment

--- frontend/src/pages/CarOverview.jsx
+++ frontend/src/pages/CarOverview.jsx
@@ -1,6 +1,5 @@
 import Header from '../components/Header'
 import { getCars } from '../BackEndConnection'
-import { Link } from 'react-router-dom' 
 
 export default function CarOverview() {
 
@@ -8,25 +7,23 @@ export default function CarOverview() {
 
   function renderCarCards() {
     return cars?.map((car, index) => {
-      const carLink = `/Car`; 
 
       return (
-        <Link key={index} to={carLink} className='mx-8 my-8 block'>
-          <div className="bg-lightBg rounded-lg overflow-hidden shadow-md">
-            <img
-              className="w-full max-h-48 object-contain" 
-              src={`./src/assets/cars/${car.manufacturer.replace(/\s+/g, '')}${car.model.replace(/\s+/g, '')}.png`}
-              alt={`${car.manufacturer} ${car.model} image`}
-            />
-            <div className="p-4">
-              <h3 className="text-lg font-medium text-white">{`${car.manufacturer} ${car.model}`}</h3>
-              <p className="text-gray-400 text-sm mb-2">{`${car.year}`}</p>
-              <div className="flex justify-between items-center">
-                <span className="text-white font-bold">{`$ ${car.price}`}</span>
-              </div>
+        <div key={index} className="bg-lightBg rounded-lg overflow-hidden shadow-md mx-8 my-8 block">
+          <img
+            className="w-full max-h-48 object-contain" 
+            src={`./src/assets/cars/${car.manufacturer.replace(/\s+/g, '')}${car.model.replace(/\s+/g, '')}.png`}
+            alt={`${car.manufacturer} ${car.model} image`}
+          />
+          <div className="p-4">
+            <h3 className="text-lg font-medium text-white">{`${car.manufacturer} ${car.model}`}</h3>
+            <p className="text-gray-400 text-sm mb-2">{`${car.year}`}</p>
+            <div className="flex justify-between items-center">
+              <span className="text-white font-bold">{`$ ${car.price}`}</span>
+              <button className='bg-yellow-500 text-white px-6 py-3 rounded-md hover:bg-yellow-600 self-end'>Select</button>
             </div>
           </div>
-        </Link>
+        </div>
       )
     })
   }
