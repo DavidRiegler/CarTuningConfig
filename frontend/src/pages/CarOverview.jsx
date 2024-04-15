e3574c1f3e91fdfa2361cf5e841dea08b08df13d
Loris Rüegg <lorisrueegg@icloud.com>
2024-04-15 17:56:09 +0200

    made colors tailwind variables

--- frontend/src/pages/CarOverview.jsx
+++ frontend/src/pages/CarOverview.jsx
@@ -24,10 +24,10 @@ export default function CarOverview() {
 
           return (
             <div key={index} className='grid grid-cols-2 grid-rows-1 w-full items-center place-items-center'>
-              <h2 className='text-lg font-semibold text-white'>{key.charAt(0).toUpperCase() + key.slice(1)}</h2>
+              <h2 className='text-lg font-semibold text-font'>{key.charAt(0).toUpperCase() + key.slice(1)}</h2>
               <div className='relative w-full'>
-                <div className={`bg-black h-2 absolute rounded-full z-20 flex justify-start`} style={{ width: '100%' }}>
-                  <div className={`bg-yellow-500 h-2 absolute rounded-full z-20 flex justify-end`} style={{ width: baseValue + '%' }}>
+                <div className={`bg-darkBg h-2 absolute rounded-full z-20 flex justify-start`} style={{ width: '100%' }}>
+                  <div className={`bg-primary h-2 absolute rounded-full z-20 flex justify-end`} style={{ width: baseValue + '%' }}>
                     <div className={`h-2 absolute rounded-full z-30`} style={{ width: percentage + '%', display: percentage < 100 ? 'block' : 'none' }} />
                   </div>
                 </div>
@@ -41,22 +41,22 @@ export default function CarOverview() {
 
   function renderCarCards() {
     return cars?.map((car, index) => (
-      <div key={index} className="grid grid-cols-2 gap-4 bg-lightBg rounded-lg overflow-hidden shadow-md mx-8 my-8 block">
+      <div key={index} className="grid grid-cols-2 gap-4 bg-lightBg rounded-lg overflow-hidden shadow-md mx-8 my-10">
         <div className="col-span-1 flex flex-col items-center justify-center">
           <img
             className="w-full max-h-48 object-contain"
             src={`./src/assets/cars/${car.manufacturer.replace(/\s+/g, '')}${car.model.replace(/\s+/g, '')}.png`}
             alt={`${car.manufacturer} ${car.model} image`}
           />
-          <h3 className="text-lg font-medium text-white mt-4">{`${car.manufacturer} ${car.model}`}</h3>
-          <p className="text-white text-sm mb-2">{`${car.year}`}</p>
+          <h3 className="text-lg font-medium text-font mt-4">{`${car.manufacturer} ${car.model}`}</h3>
+          <p className="text-font text-sm mb-2">{`${car.year}`}</p>
         </div>
 
         <div className="col-span-1 p-4 flex flex-col justify-between">
           {renderProgressBars(car)}
           <div className="flex justify-between items-center mb-4">
-            <span className="text-white font-bold mx-28">{`$ ${car.price}`}</span>
-            <button className='bg-yellow-500 text-white px-6 py-3 rounded-md hover:bg-yellow-600'>Select</button>
+            <span className="text-font font-bold mx-28">{`$ ${car.price}`}</span>
+            <button className='bg-primary text-darkBg px-6 py-3 rounded-md hover:bg-primaryHover transition-colors'>Select</button>
           </div>
         </div>
       </div>
